445eee7aca0584e76aafb8c082c014f484e2f617
Sergio <23checo06@gmail.com>
2025-10-28 19:29:46 -0600

    Initial Commit

---
+++ TodoistAppDB/TodoistAppDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoistAppDBApp.swift
+//  TodoistAppDB
+//
+//  Created by Sergio Rodríguez Pérez on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoistAppDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
